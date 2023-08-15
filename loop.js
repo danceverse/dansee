9444176a24b4ddcaba79323e585a288e915c8090
Ming <dingmo@uw.edu>
2023-08-14 23:04:17 -0700

    Save 5

--- loop.js
+++ loop.js
@@ -1,8 +1,37 @@
 var player = videojs('videoPlayer');
-var myButton = player.controlBar.addChild('button', {}, 1);
-var myButtonDom = myButton.el();
-myButtonDom.innerHTML = '<span class="vjs-icon-repeat"></span>';
+var startLoopTime = null;
+var endLoopTime = null;
+var isLooping = false;
+var loopCounter = 0;
+var loopButton = player.controlBar.addChild('button', {}, 1); // Add loop button
 
-myButtonDom.onclick = function() {
-    
-}
+// loopButton.el().innerHTML = '<span class="vjs-icon-repeat"></span>';
+loopButton.el().innerHTML = '<i class="material-icons">loop</i>';
+loopButton.el().style.fontSize = '1.5em';
+
+loopButton.on('click', function () {
+    console.log('you just clicked the button!');
+    loopCounter++;
+
+    if (loopCounter === 3) { // Check if the third click
+        isLooping = false;
+        startLoopTime = null;
+        endLoopTime = null;
+        loopCounter = 0; // Reset the loop counter
+    } else if (!isLooping) {
+        startLoopTime = player.currentTime();
+        isLooping = true;
+    } else if (startLoopTime !== null) {
+        endLoopTime = player.currentTime();
+    }
+});
+
+player.on('timeupdate', function () {
+    console.log('the player tryna loop rn!');
+    if (isLooping && startLoopTime !== null && endLoopTime !== null) {
+        if (player.currentTime() >= endLoopTime) {
+            player.currentTime(startLoopTime);
+        }
+    }
+});
+
